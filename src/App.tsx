64cd13c850672911d59984fa1390f9efa3cb24ce
unknown <kavyaaaaaa72@gmail.com>
2025-07-02 23:56:11 +0530

    Added sorting feature

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ShoppingListForm from "./pages/ShoppingListForm";
+import ShopListItemPage from "./pages/ShopListItemPage";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
           <Route path="/shopping-list" element={<ShoppingListForm />} />
+          <Route path="/shop-list/:item" element={<ShopListItemPage />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
